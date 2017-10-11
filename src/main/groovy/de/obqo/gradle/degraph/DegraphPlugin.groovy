5e553d9c296558ea4efa87abc451972bea6b0a32
Oliver Becker <ob@obqo.de>
2017-10-11 21:57:31 +0200

    Fixed degraph task dependency on sourceSets classes

--- src/main/groovy/de/obqo/gradle/degraph/DegraphPlugin.groovy
+++ src/main/groovy/de/obqo/gradle/degraph/DegraphPlugin.groovy
@@ -24,15 +24,6 @@ class DegraphPlugin implements Plugin<Project> {
         degraphTask.configuration = configuration
 
         project.afterEvaluate {
-            // make task check depend on degraph
-            project.tasks
-                    .matching { it.name.equals JavaBasePlugin.CHECK_TASK_NAME }
-                    .all { it.dependsOn degraphTask }
-            // make task degraph depend on compileJava (because it uses the classpath, see below)
-            project.tasks
-                    .matching { it.name.equals "compileJava" }
-                    .all { degraphTask.dependsOn it }
-
             // configure classpath for degraph (default: output of all source sets)
             def sources = configuration.sourceSets
             if (sources.empty) {
@@ -42,6 +33,14 @@ class DegraphPlugin implements Plugin<Project> {
             degraphTask.classpath = sources
                     .collect { it.output }
                     .inject { classpath, output -> classpath + output }
+
+            // add a dependency to each sourceSet's 'classes' task
+            sources.forEach {
+                degraphTask.dependsOn project.tasks[it.classesTaskName]
+            }
+
+            // finally make task 'check' depend on degraph
+            project.tasks[JavaBasePlugin.CHECK_TASK_NAME].dependsOn degraphTask
         }
     }
 }
